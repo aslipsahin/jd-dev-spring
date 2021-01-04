1baa35e04efc0c8e094ab494ac9e0ec232659e46
aslipsahin <aslipsahin@gmail.com>
2021-01-03 18:18:36 -0600

    Spring-11-ORM-CinemaLab-Using-Repositories module is created

---
+++ Spring-11-ORM-CinemaLab_Using_Repositories/src/main/java/com/cyber/Spring11OrmCinemaLabUsingRepositoriesApplication.java
@@ -0,0 +1,13 @@
+package com.cyber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring11OrmCinemaLabUsingRepositoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring11OrmCinemaLabUsingRepositoriesApplication.class, args);
+	}
+
+}
---
+++ Spring-11-ORM-CinemaLab_Using_Repositories/src/main/java/com/cyber/entity/Movie.java
@@ -0,0 +1,57 @@
+package com.cyber.entity;
+
+import com.cyber.enums.MovieState;
+import com.cyber.enums.MovieType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Movie extends BaseEntity {
+
+    private String name;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate releaseDate;
+
+    private Integer duration;
+
+    //    to be unable to restrict the index number for String, i will use "text" as columnDefinition
+    @Column(columnDefinition = "text")
+    private String summary;
+
+    @Enumerated(EnumType.STRING)
+    private MovieType type;
+
+    @Enumerated(EnumType.STRING)
+    private MovieState state;
+
+    private BigDecimal price;
+
+    //    when i use data.sql to generate data, i do not need cascading and constructors in both classes, but
+//      i MIGHT need it if i use DataGenerator
+    @ManyToMany
+    @JoinTable(name = "movie_genre_rel",
+            joinColumns = @JoinColumn(name = "movie_id"), inverseJoinColumns = @JoinColumn(name = "genre_id"))
+    private List<Genre> genreList = new ArrayList<>();
+
+    public Movie(String name, LocalDate releaseDate, Integer duration, String summary, MovieType type,
+                 MovieState state, BigDecimal price) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+        this.summary = summary;
+        this.type = type;
+        this.state = state;
+        this.price = price;
+    }
+}
