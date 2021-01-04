78f9e78188ee689b10049fe44614318de1139f3a
aslipsahin <aslipsahin@gmail.com>
2021-01-03 18:09:32 -0600

    Spring-11-ORM-CinemaLab module created, DB is created, data pumped by using data.sql

---
+++ Spring-11-ORM-CinemaLab/src/main/java/com/cyber/Spring11OrmCinemaLabApplication.java
@@ -0,0 +1,13 @@
+package com.cyber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring11OrmCinemaLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring11OrmCinemaLabApplication.class, args);
+	}
+
+}
---
+++ Spring-11-ORM-CinemaLab/src/main/java/com/cyber/entity/Cinema.java
@@ -0,0 +1,29 @@
+package com.cyber.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class Cinema extends BaseEntity {
+
+    private String name;
+    private String sponsoredName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+    public Cinema(String name, String sponsoredName) {
+        this.name = name;
+        this.sponsoredName = sponsoredName;
+    }
+}
