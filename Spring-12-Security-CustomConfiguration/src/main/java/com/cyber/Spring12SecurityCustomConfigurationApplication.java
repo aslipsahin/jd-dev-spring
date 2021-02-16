ae459962cd9e36fb2b27d79840435584cfe42c57
aslipsahin <aslipsahin@gmail.com>
2021-02-15 22:33:33 -0600

    Spring-12-Security-CustomConfiguration module is added

---
+++ Spring-12-Security-CustomConfiguration/src/main/java/com/cyber/Spring12SecurityCustomConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.cyber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring12SecurityCustomConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring12SecurityCustomConfigurationApplication.class, args);
+    }
+
+}
